04ff1d627921aa5c4d2f9faff070745628735776
marvelfrozen <marvelfrozen@gmail.com>
2023-10-18 16:40:11 +0700

    add restassured and junit dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+  kotlin("jvm") version "1.8.10"
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+
+  implementation("io.rest-assured:rest-assured:5.3.2")
+}
